6f73237e389c8f52377b8d0565557185f0e82510
Yasmin Mendes <yasminmendesdesouza2023@gmail.com>
2025-03-31 17:39:30 -0300

    Add files via upload

---
+++ Labirinto.swift
@@ -0,0 +1,22 @@
+//
+//  Labirinto.swift
+//  projetoJoguinho
+//
+//  Created by Aluno Mack on 24/03/25.
+//
+
+import Foundation
+
+struct Labirinto {
+    var pergunta : String
+    var mapa : [[Int]]
+    var correta : Int
+}
+
+struct Colors {
+    static let yellow = "\u{001B}[0;33m"
+    static let cyan = "\u{001B}[0;36m"
+    static let reset = "\u{001B}[0;0m"
+}
+
+
